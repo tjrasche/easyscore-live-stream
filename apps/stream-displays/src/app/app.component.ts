51d3b8e50a0f6c3c134185a175687d43b22e8cff
tjark <t@rasche-thalhofer.cloud>
2021-05-13 12:35:58 +0200

    initial commit

---
+++ apps/stream-displays/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'easy-score-livestream-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'stream-displays';
+}
